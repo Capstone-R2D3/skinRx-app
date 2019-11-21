3b6990b774a78735df5b7a3568679160b14ab282
Sylvana Santos <sylvana.e.santos@gmail.com>
2019-11-21 16:27:22 -0500

    feat: added barcode scanner view

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function ScanScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        
+// ScanScreen is a stateful component
+export default class ScanScreen extends React.Component {
+  // it has two properties on its state
+  // hasCameraPermissions and scanned
+  state = {
+    hasCameraPermission: null,
+    scanned: false,
+  };
 
-      </ScrollView>
+  // once the component is mounted, 
+  // get camera permissions from the user
+  async componentDidMount() {
+    this.getPermissionsAsync();
+  }
 
-    </View>
-  );
+  getPermissionsAsync = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    // if the user grants camera permissions, 
+    // set the hasCameraPermissions status to granted
+    this.setState({ hasCameraPermission: status === 'granted' });
+  };
+
+  render() {
+    // get the component's state
+    const { hasCameraPermission, scanned } = this.state;
+
+    // if camera permissions have not been set, 
+    // return a text view requesting camera permissions
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    // if we don't have camera permissions
+    // return a text view saying we don't have access
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
+    // otherwise, return a view with our barcode scanner
+    return (
+      <View
+        style={styles.barcodeView}>
+        <BarCodeScanner
+          // if something has been scanned, handle the scanned bar code
+          onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+        { // if user taps to scan again, set scanned state to false
+        scanned && (
+          <Button title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />
+        )
+        }
+      </View>
+    );
+  }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    // set scanned state to true
+    this.setState({ scanned: true });
+    // send user alert with barcode type and data
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    console.log();
+  };
 }
 
 ScanScreen.navigationOptions = {
@@ -28,11 +71,9 @@ ScanScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-  container: {
+  barcodeView: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
-  contentContainer: {
-    paddingTop: 30,
-  }
 });
