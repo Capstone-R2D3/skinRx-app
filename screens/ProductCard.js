913eaab9c3c21ace6b10076c4e31a7b82a393575
Athena Chen <athenachenn@gmail.com>
2019-11-22 15:38:02 -0500

    cleaned code for products and created separate product card

---
+++ screens/ProductCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import Button from 'apsl-react-native-button';
+import { withNavigation } from "react-navigation";
+
+
+
+class ProductCard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        imageUrl: this.props.state.imageUrl,
+        name: this.props.state.name, 
+        brand: this.props.state.brand,
+    }
+  }
+
+  render() {
+    return (
+      <View>
+
+          <Image source={{uri: this.state.imageUrl}}
+                 style={{width: 300, height: 300}} />
+
+          <TouchableOpacity> 
+                    <Button textStyle={{color: 'grey'}} style={{borderWidth: 1, borderColor: 'grey', borderRadius:10}} 
+                        onPress={() => {
+                          this.props.navigation.navigate("SingleProduct", {
+                            imageUrl: this.state.imageUrl,
+                            name: this.state.name, 
+                            brand: this.state.brand,
+                          });
+                        }}
+                        >View Product</Button>
+          </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+export default withNavigation(ProductCard);
