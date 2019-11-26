effef9ca55be6ed299b1784c63285cfbd47416eb
sjuhye <sjuhye@gmail.com>
2019-11-26 17:15:11 -0500

    added styles to JourneyCard

--- screens/JourneyCard.js
+++ screens/JourneyCard.js
@@ -14,3 +14,41 @@ class JourneyCard extends Component {
 }
 
 export default JourneyCard;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "white"
+    },
+    header: {
+      textAlign: "center",
+      fontSize: 30,
+      marginBottom: 20
+    },
+    btnContainer: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    userBtn: {
+      backgroundColor: "#dadada",
+      padding: 15,
+      width: "75%",
+      display: "flex",
+      borderRadius: 7,
+      margin: 5
+    },
+    btnText: {
+      fontSize: 18,
+      textAlign: "center"
+    },
+    bodyText: {
+        fontSize: 12,
+        textAlign: "center",
+        margin: 15
+    }
+});
