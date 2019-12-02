033a1e818c265362979f60639945b00486fd4761
Athena Chen <athenachenn@gmail.com>
2019-12-02 13:57:16 -0500

    navigation bar for recommended products uploaded

--- screens/RecommendationScreen.js
+++ screens/RecommendationScreen.js
@@ -7,11 +7,14 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions,
 } from 'react-native';
 import ProductCard from './ProductCard';
 import {connect} from 'react-redux'
 import {getRecommendations} from '../redux/reducers/recommendations'
 import { ThemeConsumer } from 'react-native-elements';
+
+let scrollYPos = 0;
 
 class RecommendationScreen extends React.Component {
   constructor(props) {
@@ -19,15 +22,44 @@ class RecommendationScreen extends React.Component {
     this.state = {
       userId: this.props.user.id,
       skinTypeId: this.props.user.skinTypeId, 
-      current: 'cleanser'
+      selected: 'cleanser',    
+      // screenWidth: Dimensions.get('window').width,
+      screenWidth: 330,
     }
+    this.scrollToA = this.scrollToA.bind(this)
+    this.scrollToB = this.scrollToB.bind(this)
+    this.scrollToC = this.scrollToC.bind(this)
+    this.scrollToD = this.scrollToD.bind(this)
   }
 
   async componentDidMount() {
     await this.props.getRecommendations(this.state.userId, this.state.skinTypeId)
   }
 
+  // scrolling functionality
+  scrollToA = () => {
+    this.scroller.scrollTo({x: 0, y: 0});
+    this.setState({selected: "cleanser"})
+  };
+  scrollToB = () => {
+    scrollXPos = this.state.screenWidth * 1;
+    this.scroller.scrollTo({x: scrollXPos, y: 0});
+    this.setState({selected: "toner"})
+  };
+  scrollToC = () => {
+    scrollXPos = this.state.screenWidth * 2;
+    this.scroller.scrollTo({x: scrollXPos, y: 0});
+    this.setState({selected: "serum"})
+  };
+  scrollToD = () => {
+    scrollXPos = this.state.screenWidth * 3;
+    this.scroller.scrollTo({x: scrollXPos, y: 0});
+    this.setState({selected: "moisturizer"})
+  };
+  
+
   render() {
+
     return (
       <View style={styles.container} contentContainerStyle={styles.contentContainer}>
           
@@ -35,10 +67,18 @@ class RecommendationScreen extends React.Component {
           
           {/* <Text></Text> */}
           <View style={{display: "flex", flexDirection: "row", justifyContent: "space-evenly", marginTop: 25}}>
-            <Text value={'cleanser'}>Cleanser</Text>
-            <Text value={'toner'}>Toner</Text>
-            <Text value={'serum'}>Serum</Text>
-            <Text value={'moisturizer'}>Moisturizer</Text>
+            <TouchableOpacity onPress={this.scrollToA}>
+              <Text style={ this.state.selected === 'cleanser' ? styles.clicked : '' }>Cleanser</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.scrollToB}>
+              <Text style={ this.state.selected === 'toner' ? styles.clicked : '' }>Toner</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.scrollToC}>
+              <Text style={ this.state.selected === 'serum' ? styles.clicked : '' }>Serum</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={this.scrollToD}>
+              <Text style={ this.state.selected === 'moisturizer' ? styles.clicked : '' }>Moisturizer</Text>
+            </TouchableOpacity>
           </View>
 
           <ScrollView 
@@ -51,29 +91,25 @@ class RecommendationScreen extends React.Component {
               showsHorizontalScrollIndicator={false}
               scrollEventThrottle={10}
               pagingEnabled
-              // onScroll={}
+              ref={(scroller) => {this.scroller = scroller}}
           >
-            
+
           {/* Product no 1 - cleanser */}
-          {/* <Text style={styles.text}>Cleanser</Text> */}
          { this.props.recommendations.recommendations.length > 0 ? <ProductCard state={this.props.recommendations.recommendations[0].cleanser[0]} /> : null }
 
           <Text></Text>
           
           {/* Product no 2 - toner */}
-          {/* <Text style={styles.text}>Toner</Text> */}
           { this.props.recommendations.recommendations.length > 0 ? <ProductCard state={this.props.recommendations.recommendations[0].toner[0]}  /> : null }
 
           <Text></Text>
 
           {/* product no. 3 - moisturizer */}
-          {/* <Text style={styles.text}>Moisturizer</Text> */}
           { this.props.recommendations.recommendations.length > 0 ? <ProductCard state={this.props.recommendations.recommendations[0].moisturizer[0]}  /> : null }
 
           <Text></Text>
 
           {/* product no. 4 - sunscreen */}
-          {/* <Text style={styles.text}>Serum</Text> */}
           { this.props.recommendations.recommendations.length > 0 ? <ProductCard state={this.props.recommendations.recommendations[0].serum[0]}  /> : null }
 
           <Text></Text>
@@ -84,9 +120,11 @@ class RecommendationScreen extends React.Component {
   }
 }
 
+
 RecommendationScreen.navigationOptions = {
   title: 'Recommended Products',
 };
+
 
 const styles = StyleSheet.create({
   header: {
@@ -102,13 +140,22 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 10,
     paddingHorizontal: 15,
-    backgroundColor: "#e1e9f0",
+    // backgroundColor: "#e1e9f0",
+    backgroundColor: "#e6ebf0"
   },
   contentContainer: {
     paddingBottom: 75,
     paddingHorizontal: 7,
+  }, 
+  clicked: {
+    color: "white", 
+    fontWeight: "bold",
+    borderBottomWidth: 2, 
+    borderBottomColor: "white",
+    // fontSize: 15,
   }
 })
+
 
 const mapState = state => ({
   recommendations: state.recommendations,
