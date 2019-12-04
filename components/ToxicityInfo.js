3de71c7ff3cfa50a6fba12e9ea54c834eda08df6
Andi Plummer <andrea@andreaplummer.com>
2019-12-04 16:59:00 -0500

    fixed minor bugs and layouts

--- components/ToxicityInfo.js
+++ components/ToxicityInfo.js
@@ -11,7 +11,26 @@ import {
 
 export default ToxicityInfo = ({ label }) => (
   <View>
-    <Text>What is a toxicity score?</Text>
-    <Text>How is toxicity score calculated?</Text>
+    {/* <View>
+      <Text>What is a toxicity score?</Text>
+      <Text></Text>
+    </View>
+    <View>
+      <Text>How is toxicity score calculated?</Text>
+      <Text>There are two different scores that are used to determine a product's score - the hazard score and the data score. 
+        Each product is assigned a score based on toxicity level, ingredient interaction, concentration, and scientific research. 
+
+
+      </Text>
+    </View>
+    <View>
+      <Text>What does each score mean?</Text>
+      <Text>1 - 2</Text>
+      <Text>Low toxicity</Text>
+      <Text>3 - 6</Text>
+      <Text>Low toxicity</Text><
+        Text>1 - 2</Text>
+      <Text>Low toxicity</Text>
+    </View> */}
   </View>
 )
